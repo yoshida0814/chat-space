0736b0cf4f1208a482c95829665b82af1270ad0e
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-11-05 20:47:52 +0900

    インクリメンタルサーチ兼自動更新1回目

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,15 @@
 $(document).on('turbolinks:load', function(){
   function buildHTML(message) {
+    console.log(message)
     var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+    var img = message.image.url ? `<img src= ${ message.image.url }>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__detail">
                     <p class="message__detail__current-user-name">
                       ${message.user_name}
                     </p>
                     <p class="message__detail__date">
-                      ${message.date}
+                      ${message.created_at}
                     </p>
                   </div>
                   <p class="message_body">
@@ -23,11 +24,12 @@ $(document).on('turbolinks:load', function(){
   return html;
 
   }
-
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
     var url = (window.location.href);
+    // var url = $(this).attr('action')
+    // console.log(url)
     $.ajax({  
       url: url,
       type: 'POST',
@@ -51,4 +53,38 @@ $(document).on('turbolinks:load', function(){
     })
   })
 
+ 　　 var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id =  $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages", 
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      console.log(messages)
+      var insertHTML = '';
+      messages.forEach(function(message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML); 
+        $('.content__right__center').animate({scrollTop: $('.content__right__center')[0].scrollHeight}, 'fast');
+
+      });
+     
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      //メッセージが入ったHTMLを取得
+      //メッセージを追加
+    })
+    .fail(function() {
+      //console.log('error');
+    });
+    
+  };
+
+  setInterval(reloadMessages, 1000);
 });
