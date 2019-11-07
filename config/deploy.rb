1f0073ecaaee430d3f4da74e041a47b8d0a10a55
yoshida0814 <shin.yoshida.0814@outlook.com>
2019-11-07 14:57:24 +0900

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:yoshida0814/chat-space.git"
+set :repo_url, "git@github.com:yoshida0814/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
